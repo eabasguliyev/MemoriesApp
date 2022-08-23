8cd5dd05cb89dc27a6a532629447279b779e54e7
Elgun <elgun.abasquliyev@gmail.com>
2022-08-23 16:15:50 +0400

    Redux

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const uri = "http://localhost:5000/posts";
+const url = "http://localhost:5000/posts";
 
 const fetchPosts = () => axios.get(url);
 
--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -1,10 +1,112 @@
-import React from "react";
+import React, { useState } from "react";
+import { TextField, Button, Typography, Paper } from "@material-ui/core";
 import useStyles from "./styles";
+import FileBase from "react-file-base64";
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: "",
+    title: "",
+    message: "",
+    tags: "",
+    selectedFile: "",
+  });
+
   const classes = useStyles();
 
-  return <h1>FORM</h1>;
+  const handleSubmit = () => {};
+
+  const clear = () => {};
+  return (
+    <Paper className={classes.paper}>
+      <form
+        autoComplete="off"
+        noValidate
+        className={`${classes.root} ${classes.form}`}
+        onSubmit={handleSubmit}
+      >
+        <Typography variant="h6">Creating a Memory</Typography>
+        <TextField
+          name="creator"
+          variant="outlined"
+          label="Creator"
+          fullWidth
+          value={postData.creator}
+          onChange={(e) =>
+            setPostData((oldData) => {
+              return { ...oldData, creator: e.target.value };
+            })
+          }
+        />
+        <TextField
+          name="title"
+          variant="outlined"
+          label="Title"
+          fullWidth
+          value={postData.title}
+          onChange={(e) =>
+            setPostData((oldData) => {
+              return { ...oldData, title: e.target.value };
+            })
+          }
+        />
+        <TextField
+          name="message"
+          variant="outlined"
+          label="Message"
+          fullWidth
+          value={postData.message}
+          onChange={(e) =>
+            setPostData((oldData) => {
+              return { ...oldData, message: e.target.value };
+            })
+          }
+        />
+        <TextField
+          name="tags"
+          variant="outlined"
+          label="Tags"
+          fullWidth
+          value={postData.tags}
+          onChange={(e) =>
+            setPostData((oldData) => {
+              return { ...oldData, tags: e.target.value };
+            })
+          }
+        />
+        <div className={classes.fileInput}>
+          <FileBase
+            type="file"
+            multiple={false}
+            onDone={({ base64 }) => {
+              setPostData((oldData) => {
+                return { ...oldData, selectedFile: base64 };
+              });
+            }}
+          ></FileBase>
+        </div>
+        <Button
+          className={classes.buttonSubmit}
+          variant="contained"
+          color="primary"
+          sizie="large"
+          type="submit"
+          fullWidth
+        >
+          Submit
+        </Button>
+        <Button
+          variant="contained"
+          color="secondary"
+          sizie="small"
+          onClick={clear}
+          fullWidth
+        >
+          Clear
+        </Button>
+      </form>
+    </Paper>
+  );
 };
 
 export default Form;
